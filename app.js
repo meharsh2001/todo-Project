7b1a8267a57e3f31dd49e31ae1e315b9eb53da4a
meharsh2001 <meharsh2001@gmail.com>
2022-07-07 16:38:01 +0530

    main

--- app.js
+++ app.js
@@ -1,51 +1,43 @@
 var express         =require("express"),
-    mongoose        =require("mongoose"),
     port            =process.env.PORT || 8000,
     app             =express(),
-
-    methodOverride  =require("method-override")
-    passport        =require("passport"),
-    LocalStrategy   =require("passport-local"),
-    User            =require("./models/user"),
-    middleware      =require("./middleware/middleware.js"),
+    methodOverride  =require("method-override"),
     bodyParser      =require("body-parser")
 
     app.use(methodOverride("_method"));
     app.use(bodyParser.urlencoded({extended:false}));
 
-var indexRoute=require("./routes/index")
 
 app.use(express.static(__dirname + "/public"));
 app.set("view engine","ejs");
 
 
-//ONLINE MONGODB ATLAS
-const DB='mongodb+srv://admin:admin@cluster0.oun5l.mongodb.net/new';
-mongoose.connect(DB).then(()=>{console.log('DATABASE CONNECTED to localhost:'+ port);}).catch((err)=> console.log('Database Not Connected'+err));
-
-//Offline MONGO
-//mongoose.connect("mongodb://localhost:27017/ttchannel").then( () => console.log("success")).catch((err)=>console.log(err));   
-
 app.use(require("express-session")({
-    secret:"Session for MissingX",
+    secret:"Session",
     resave:false,
     saveUninitialized:false
 }));
 
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()))
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req,res,next)
-{
-    res.locals.currentUser=req.user;
-    next(); 
+//HOME
+app.get("/",function(req,res)
+{   
+    res.render("home");
 });
-app.use(indexRoute);
+
+//MYACCOUNT    
+app.get("/myaccount",function(req,res)
+{
+    res.render("myaccount")});
+
+
+//REgISTER
+app.get("/register",function(req,res)
+{
+    res.render("register");
+});
 
 app.listen(port,function()
 {
-    console.log("server has started!");
+    console.log("http://localhost:"+port+"/");
 });
--- middleware/middleware.js
+++
@@ -1,6 +0,0 @@
-var middlewareObj = {};
-middlewareObj.isLoggedIn= function(req,res,next)
-{return next();
-}
-
-module.exports=middlewareObj;
